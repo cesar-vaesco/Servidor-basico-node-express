399a3fff592fd32ef690739fa503e79c7e477cfe
Cesar <cesarvargasescorcia@gmail.com>
2020-07-27 07:13:55 -0500

    express: estableciendo configuracion para determinar donde llamar las vistas y determinar el motor de plantillas

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const colors = require('colors');
 
-//Setting - configuraciónes
-app.set('port', process.env.PORT || 3000);
+/**Setting - configuraciónes*/
 
-//Middlewares
+app.set('port', process.env.PORT || 3000);
+//Configurar la ruta de las vistas
+app.set('views', path.join(__dirname, 'views'));
+//Configurar el motor de plantillas
+app.set('view engine', 'ejs');
+
+/**Middlewares */
+//Se indica la url y el tipo de petición que se ha solicitado " '/' - GET "
+app.use((req, res, next) => {
+    console.log(`${req.url} - ${req.method}`);
+    next();
+});
 
 
 //routes
