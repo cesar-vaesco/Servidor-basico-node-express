c0afa3e271c8d6ef5036e3aafd3886563ff9c81e
Cesar <cesarvargasescorcia@gmail.com>
2020-07-27 10:21:06 -0500

    express:crear una lista de productos y pasarlos a la vista - uso de bucle for con ejs

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,45 @@ const router = express.Router();
 //     res.end('Hello word');
 // });
 
+const items = [{
+        id: 1,
+        nombre: 'producto1'
+    },
+    {
+        id: 2,
+        nombre: 'producto2'
+    },
+    {
+        id: 3,
+        nombre: 'producto3'
+    },
+    {
+        id: 4,
+        nombre: 'pr0ducto4'
+    },
+    {
+        id: 5,
+        nombre: 'pr0ducto5'
+    },
+    {
+        id: 6,
+        nombre: 'pr0ducto6'
+    },
+
+]
+
 router.get('/', (req, res) => {
     res.render('index', {
         title: 'Mi tienda web'
+
     });
 });
 
 
 router.get('/productos', (req, res, next) => {
     res.render('productos', {
-        title: 'Lista de productos'
+        title: 'Lista de productos',
+        items: items
     })
 });
 
