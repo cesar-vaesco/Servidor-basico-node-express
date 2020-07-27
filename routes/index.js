c0cbc595045529d56cd7c98ff31c6ac490c5a827
Cesar <cesarvargasescorcia@gmail.com>
2020-07-27 07:31:27 -0500

    express: testeando la llamada a la vista index.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+/** Probando ruta  y su respuesta*/
+// router.get('/', function(req, res) {
+//     res.end('Hello word');
+// });
+
+router.get('/', function(req, res) {
+    res.render('index');
+});
+
+
 module.exports = router;
