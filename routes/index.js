ee5d6d6e7a0525396fd8b43e3064f65e96870d4f
Cesar <cesarvargasescorcia@gmail.com>
2020-07-27 14:27:29 -0500

    express: dando estilos con bootstrap

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const { index, ListaProductos, nuevoProducto } = require('../controllers/index');
-/** Probando ruta  y su respuesta*/
-// router.get('/', function(req, res) {
-//     res.end('Hello word');
-// });
-
 
 
 router.get('/', index);
